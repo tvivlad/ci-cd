555c937d316092ac8fe56c2ea46c8899051dfad3
tvivlad <tvi-vlad@mail.ru>
2024-05-13 21:07:43 +0500

    alt text error fixed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ const Card = ({ name, image, rating, count }) => {
   return (
     <div className='card'>
       <div className='title'>{name}</div>
-      <img src={image} className='img' width={200} height={200} />
+      <img src={image} className='img' alt='pizza' width={200} height={200} />
       <div className='ratAndCount'>Rating: {rating}</div>
       <div className='ratAndCount'>Count:{count}</div>
     </div>
